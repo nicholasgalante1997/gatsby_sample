d786c3e108579b5eabcfaad68e2f84ee070e3757
Nick Galante <nicholasgalante1997@gmail.com>
2021-07-20 21:29:27 -0600

    mdx content render

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,38 +1,36 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-          {data.allFile.edges.filter(edge => {
-            const markdown = /.mdx/;
-            if (edge.node.relativePath.search(markdown) !== -1){
-                return true;
-            }
-            return false;
-          }).map(markdownEdge => {
-            return (
-            <li key={markdownEdge.relativePath}>
-                {markdownEdge.node.name}
-            </li>
-        )})}
-      </ul>
+      {
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Contributed By: {node.frontmatter.author}</p>
+            <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+          </article>
+        ))
+      }
     </Layout>
   )
 }
 
 export const query = graphql`
 query {
-  allFile {
-    edges {
-      node {
-        name
-        dir
-        relativeDirectory
-        relativePath
+  allMdx {
+    nodes {
+      frontmatter {
+        author
+        title
       }
+      id
+      body
     }
   }
 }
